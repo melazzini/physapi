27ebc7e378417e8f95598d5a91892137aea20a00
franciscoMelazzini <francisco.melazzini@physics.msu.ru>
2021-02-07 21:20:42 +0300

    added psimple2dplotareacalculator

---
+++ include/core/PSimple2DPlotAreaCalculator.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "utils.hpp"
+#include <span>
+
+namespace physapi
+{
+    /**
+     * @brief This is a functor class to calculate the area below a curve(scattered),
+     * given by a set of points-coordinates.
+     * 
+     * @ingroup core
+     */
+    class PSimple2DPlotAreaCalculator
+    {
+        struct Triangle
+        {
+            phys_float a, b, c; // legs lengths
+
+            phys_float perimeter() { return a + b + c; }
+            phys_float area()
+            {
+                auto s{perimeter() / 2};
+                return std::sqrt(s * (s - a) * (s - b) * (s - c));
+            }
+        };
+
+    private:
+        phys_float m_lowerBound;
+        phys_float m_upperBound;
+
+    public:
+        /**
+         * @brief Construct a new PSimple2DPlotAreaCalculator object
+         * 
+         * @param a lower bound
+         * @param b upper bound
+         */
+        PSimple2DPlotAreaCalculator(phys_float a, phys_float b)
+            : m_lowerBound{a}, m_upperBound{b} {}
+
+        /**
+         * @brief Destroy the PSimple2DPlotAreaCalculator object
+         * 
+         */
+        virtual ~PSimple2DPlotAreaCalculator() {}
+
+        /**
+         * @brief Get the scattered plot's area
+         * 
+         * @param plot scattered plot(coordinates)
+         * @return phys_float area
+         */
+        phys_float operator()(std::span<const std::pair<phys_float, phys_float>> plot);
+    };
+} // namespace physapi
---
+++ src/core/PSimple2DPlotAreaCalculator.cpp
@@ -0,0 +1,37 @@
+#include "PSimple2DPlotAreaCalculator.hpp"
+#include <iostream>
+
+namespace physapi
+{
+    phys_float PSimple2DPlotAreaCalculator::operator()(std::span<const std::pair<phys_float, phys_float>> plot)
+    {
+        if (plot.empty() || plot.size() <= 1)
+        {
+            return 0.0;
+        }
+
+        phys_float area_tot{};
+
+        for (phys_size i = 0; i < plot.size() - 1; i++)
+        {
+            phys_float rect_base{plot[i + 1].first - plot[i].first};
+            phys_float rect_height{plot[i + 1].second};
+            if (rect_base < 0 || rect_height < 0)
+            {
+                std::cerr << "the rectangle sides cannot be zero!" << std::endl;
+                std::cerr << "but rect_base:  " << rect_base << std::endl;
+                std::cerr << "but rect_height:  " << rect_height << std::endl;
+                std::abort();
+            }
+
+            phys_float triangle_height = {std::abs(plot[i + 1].second - plot[i].second)};
+            phys_float triangle_leg = std::sqrt(sqr(rect_base) + sqr(triangle_height));
+            auto triangle = Triangle{rect_base, triangle_height, triangle_leg};
+            auto rect_area = rect_base * rect_height;
+            area_tot += (rect_area + triangle.area());
+        }
+
+        return area_tot;
+    }
+
+} // namespace physapi
